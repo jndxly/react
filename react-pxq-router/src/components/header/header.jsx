5d288854ef7c4e73d0777f750f4bed86a00133d0
chenhao03 <chenhao03@qiyi.com>
2018-09-05 23:02:04 +0800

    理解reactCssTranstionGroup的enter和leave

--- react-pxq-router/src/components/header/header.jsx
+++ react-pxq-router/src/components/header/header.jsx
@@ -1,68 +1,63 @@
-import React, {Component} from 'react';
-import {is, fromJS} from 'immutable';
-import {NavLink} from 'react-router-dom';
-import ProtoTypes from 'prop-types';
+import React, { Component } from 'react';
+import { is, fromJS } from 'immutable';
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import './header.less';
 
 export default class PublicHeader extends Component{
     static propTypes = {
-        record:ProtoTypes.any,
-        title:ProtoTypes.string.isRequired,
-        confirm:ProtoTypes.any
-    };
-
-    state = {
-        navState : false
-    };
-
-    toggleNav = ()=>{
-        this.setState({
-            navState:!this.state.navState
-        });
-    };
-
-    FirstChild = props =>{
-        const childrenArray = React.Children.toArray(props.children);
-        return childrenArray[0] || null;
-    };
-
-    shouldComponentUpdate(nextProps, nextState){
-        return !is(fromJS(this.props), fromJS(nextProps)) || !is(fromJS(this.state) , fromJS(nextState));
-    };
-
-    render(){
-
-        return (
-            <header className="header-container">
-            <span className="header-slide-icon icon-catalog" onClick={this.toggleNav}></span>
-            <span className="header-title">{this.props.title}</span>
-            {
-                this.props.record && <NavLink to="/" exact className="header-link icon-jilu"></NavLink>
-            }
-            {
-                this.props.confirm && <NavLink to="/" exact className="header-link header-link-confirm">确定</NavLink>
-            }
-
-            <ReactCSSTransitionGroup
-                component={this.FirstChild}
-                transitionName="nav" //transitionName属性定义了class的前缀
-                transitionEnterTimeout={9000}
-                transitionLeaveTimeout={9000} >
-                {
-                    this.state.navState && <aside key="nav-slide" className="nav-slide-list" onClick={this.toggleNav}>
-                        <NavLink to="/" exact className="nav-link icon-jiantou-copy-copy">首页</NavLink>
-                        <NavLink to="/balance" exact className="nav-link icon-jiantou-copy-copy">提现</NavLink>
-                        <NavLink to="/" exact className="nav-link icon-jiantou-copy-copy">帮助中心</NavLink>
-                    </aside>
-                }
-
-            </ReactCSSTransitionGroup>
-
-        </header>
-        );
-
-
+        record: PropTypes.any,
+        title: PropTypes.string.isRequired,
+        confirm: PropTypes.any,
     }
 
+    state = {
+        navState: false, //导航栏是否显示
+    };
+
+    // 切换左侧导航栏状态
+    toggleNav = () => {
+        /*为true时，为enter，此时为aside添加nav-enter nav-enter-active，因此300ms出现
+        * 为false时，为leave，磁力aside添加nav-leave nav-leave-active因此会300ms消失*/
+        this.setState({navState: !this.state.navState});
+    }
+    // css动画组件设置为目标组件
+    FirstChild = props => {
+        const childrenArray = React.Children.toArray(props.children);
+        return childrenArray[0] || null;
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+        debugger;
+        return !is(fromJS(this.props), fromJS(nextProps))|| !is(fromJS(this.state),fromJS(nextState))
+    }
+
+    render(){
+        return(
+            <header className={"header-container"}>
+                <span className="header-slide-icon icon-catalog" onClick={this.toggleNav}></span>
+                <span className="header-title">{this.props.title}</span>
+                {
+                    this.props.record&&<NavLink to="/record" exact className="header-link icon-jilu"></NavLink>
+                }
+                {
+                    this.props.confirm&&<NavLink to="/" exact className="header-link header-link-confim">确定</NavLink>
+                }
+                <ReactCSSTransitionGroup
+                    component={this.FirstChild}
+                    transitionName="nav"
+                    transitionEnterTimeout={3000}
+                    transitionLeaveTimeout={3000}>
+                    {
+                        this.state.navState && <aside key='nav-slide' className="nav-slide-list" onClick={this.toggleNav}>
+                            <NavLink to="/" exact className="nav-link icon-jiantou-copy-copy">首页</NavLink>
+                            <NavLink to="/balance" exact className="nav-link icon-jiantou-copy-copy">提现</NavLink>
+                            <NavLink to="/helpcenter" exact className="nav-link icon-jiantou-copy-copy">帮助中心</NavLink>
+                        </aside>
+                    }
+                </ReactCSSTransitionGroup>
+
+            </header>
+        );
+    }
 }
